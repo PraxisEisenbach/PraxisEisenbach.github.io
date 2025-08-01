7f7d31fedb4af2c48d0f23f40d96de6a3503f7d0
PraxisEisenbach <psychotherapie.eisenbach@gmail.com>
2025-08-01 20:14:21 +0200

    Update mobile-logo.js

--- mobile-logo.js
+++ mobile-logo.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
     logoLink.style.position = "absolute";
     logoLink.style.top = "0";
     logoLink.style.left = "0";
-    logoLink.style.height = "144px";
-    logoLink.style.width = "216px";
+    logoLink.style.height = "192px";
+    logoLink.style.width = "288px";
     logoLink.style.zIndex = "3";
     logoLink.style.backgroundImage = "url('/Logo.jpg')";
     logoLink.style.backgroundSize = "contain";
