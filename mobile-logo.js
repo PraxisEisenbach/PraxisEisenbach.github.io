5a45a0079c423e04dfbbe25885ac72d980005bd9
PraxisEisenbach <psychotherapie.eisenbach@gmail.com>
2025-08-01 19:53:49 +0200

    Update mobile-logo.js

--- mobile-logo.js
+++ mobile-logo.js
@@ -1,20 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const headerContainer = document.querySelector("header .container.mx-auto > div:nth-child(2)");
+  const headerContainer = document.querySelector("header .container.mx-auto");
 
   if (headerContainer && window.innerWidth <= 768) {
+    // Logo-Link erzeugen
     const logoLink = document.createElement("a");
     logoLink.href = "index.html";
     logoLink.style.position = "absolute";
     logoLink.style.top = "0";
     logoLink.style.left = "0";
-    logoLink.style.height = "120px";
-    logoLink.style.width = "180px";
+    logoLink.style.height = "240px";
+    logoLink.style.width = "360px";
     logoLink.style.zIndex = "3";
-    logoLink.style.backgroundImage = "url('Logo.jpg')";
+    logoLink.style.backgroundImage = "url('/Logo.jpg')";
     logoLink.style.backgroundSize = "contain";
     logoLink.style.backgroundRepeat = "no-repeat";
     logoLink.style.backgroundPosition = "top left";
     logoLink.style.display = "block";
+
     headerContainer.appendChild(logoLink);
   }
 });
